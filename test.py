88d88e9990ac5e60a065473788201b102535893b
liuxiaotong15 <liuxiaotong15@mails.ucas.ac.cn>
2021-09-25 22:45:04 +0800

    add 3d_kl func
    Signed-off-by: liuxiaotong15 <liuxiaotong15@mails.ucas.ac.cn>

--- test.py
+++ test.py
@@ -24,13 +24,30 @@ def KL_divergence(p, q):
             px = P_count[i] / sum(P_count)
             qx = Q_count[i] / sum(Q_count)
             if px != 0 and qx != 0:
-                print(px * log(px / qx))
                 s += px * log(px / qx)
         kl.append(s)
-        print(s, '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
-    print(kl)
     return kl
-    
+
+def KL_3d(p, q):
+    kl = 0
+    arr_size = 11
+    p_distr = np.zeros((arr_size, arr_size, arr_size))
+    q_distr = np.zeros((arr_size, arr_size, arr_size))
+    arr_size -= 1
+    for p_item in p:
+        p_distr[(round(p_item[0]*arr_size))][(round(p_item[1]*arr_size))][(round(p_item[2]*arr_size))] += 1
+    for q_item in q:
+        q_distr[(round(q_item[0]*arr_size))][(round(q_item[1]*arr_size))][(round(q_item[2]*arr_size))] += 1
+    arr_size += 1
+    for i in range(arr_size):
+        for j in range(arr_size):
+            for k in range(arr_size):
+                px = p_distr[i][j][k] / np.sum(p_distr)
+                qx = q_distr[i][j][k] / np.sum(q_distr)
+                if px != 0 and qx != 0:
+                    kl += px * log(px / qx)
+    return kl
+
 df_name = [['P', 'S', 'H', 'G'], ['PS', 'PH', 'PG', 'SH',	 'SG', 'HG'], ['PSH', 'PSG', 'PHG', 'SHG'], ['PSHG']]
 col_name = {'P': 'pbe', 'S': 'scan', 'H': 'hse', 'G': 'gllb-sc'}
 
@@ -44,24 +61,28 @@ col = ['P-SHG', 'S-PHG', 'H-PSG', 'G-PSH']
 row = [' pca_0 ', ' pca_1 ', ' pca_2 ']
 value = np.zeros((3, 4))
 
+print(pca_dict.keys())
+
 for i, left in enumerate(df_name[0]):
     p = pca_dict[left]
     q = []
     for key in pca_dict.keys():
         if left != key:
             q.extend(pca_dict[key])
-    value[0][i], value[1][i], value[2][i] = KL_divergence(p, q)
-    
-plt.figure(figsize=(12,6))
-tab = plt.table(cellText = value, 
-              colLabels = col, 
-              rowLabels = row,
-              loc = 'center', 
-              cellLoc = 'center',
-              rowLoc = 'center')
-tab.auto_set_font_size(False)
-tab.set_fontsize(10)
-# tab.scale(1,1.5) 
-plt.subplots_adjust(bottom=0.040, right=0.995, left=0.055, top=1.000)
-plt.axis('off')
-plt.show()
+    # value[0][i], value[1][i], value[2][i] = KL_divergence(p, q)
+    value[0][i] = KL_3d(p, q)
+print(value)
+
+# plt.figure(figsize=(12,6))
+# tab = plt.table(cellText = value, 
+#               colLabels = col, 
+#               rowLabels = row,
+#               loc = 'center', 
+#               cellLoc = 'center',
+#               rowLoc = 'center')
+# tab.auto_set_font_size(False)
+# tab.set_fontsize(10)
+# # tab.scale(1,1.5) 
+# plt.subplots_adjust(bottom=0.040, right=0.995, left=0.055, top=1.000)
+# plt.axis('off')
+# plt.show()
